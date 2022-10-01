7ce37eb6c1df668c5b9a24b0f5d3ed774124f7e3
이경준 <>
2022-10-01 11:11:42 +0900

    eslint prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'no-alert': 'off',
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
+    'import/prefer-default-export': 'off',
+    'jsx-a11y/no-autofocus': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+};
